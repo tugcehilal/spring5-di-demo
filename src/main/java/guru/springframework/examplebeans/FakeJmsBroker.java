7f62353b95df96886edfca79b196f930561cecc0
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-24 00:05:05 +0300

    external configurations

---
+++ src/main/java/guru/springframework/examplebeans/FakeJmsBroker.java
@@ -0,0 +1,33 @@
+package guru.springframework.examplebeans;
+
+public class FakeJmsBroker {
+    private String username;
+    private String password;
+    private String URL;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+}
+
+
